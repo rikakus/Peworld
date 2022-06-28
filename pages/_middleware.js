da51d1cec56ae1cfafdfdef24cb48e14546329b2
rikakus <ianpangestu082@gmail.com>
2022-06-28 13:26:35 +0700

    update middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,8 +2,20 @@ import { NextResponse } from "next/server";
 
 export default function middleware(req) {
   const { token } = req.cookies;
-  const {pathname,origin} = req.nextUrl
-  if(!token && pathname !== "/login" && pathname !== "/register" && pathname !== "/register/worker" && pathname !== "/register/recruiter" && pathname !== "/" && pathname !== "/home" && pathname !== "/next2.svg" && pathname !== "/next1.svg"){
-      return NextResponse.redirect(`${origin}/login`)
+  const { pathname, origin } = req.nextUrl;
+  if (
+    !token &&
+    pathname !== "/login" &&
+    pathname !== "/register" &&
+    pathname !== "/register/worker" &&
+    pathname !== "/register/recruiter" &&
+    pathname !== "/" &&
+    pathname !== "/home" &&
+    pathname !== "/next2.svg" &&
+    pathname !== "/next1.svg" &&
+    pathname !== "/logo.png" &&
+    pathname !== "/logo2.png"
+  ) {
+    return NextResponse.redirect(`${origin}/login`);
   }
 }
